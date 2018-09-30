db82d9f4dff78166aa1e8d7aae9c0fb567980699
Ans Riaz <ansriazch@hotmail.com>
2018-09-30 16:53:34 +0200

    profile model added

---
+++ models/profile.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const logger = require('../helper/logger')
+const errors = require('../helper/errors')
+
+const Profile = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+        default: require('uuid/v4'),
+        index: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: String
+    }
+}, {
+    toJSON: {
+        transform: function (doc, ret) {
+            delete ret._id
+            delete ret.__v
+        }
+    }
+})
+
+Profile.methods.merge = function(p) {
+
+    const prof = this
+
+    return Promise.resolve()
+        .then(() => {
+
+            if (p.userId) {
+                prof.userId = p.userId
+            }
+
+            if (p.name) {
+                prof.name = p.name
+            }
+
+            if(p.value) {
+                prof.value = p.value
+            }
+
+            return Promise.resolve()
+        })
+        .then(() => {
+            return Promise.resolve(prof)
+        })
+}
+
+module.exports = mongoose.model('Profile', Profile)
